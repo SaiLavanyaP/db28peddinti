077ab29fb105142e62c6107707bd669db883b4b6
SaiLavanyaP <peddinti98lavanya@gmail.com>
2021-11-10 21:10:17 -0600

    added

--- controllers/dog.js
+++ controllers/dog.js
@@ -1,8 +1,15 @@
 var dog = require('../models/dog');
 // List of all dog
-exports.dog_list = function(req, res) {
- res.send('NOT IMPLEMENTED: dog list');
-};
+exports.dog_list = async function(req, res) {
+    try{
+    thedog = await dog.find();
+    res.send(thedog);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
 // for a specific dog.
 exports.dog_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: dog detail: ' + req.params.id);
