1e8c1ede47f8f285ea537cbbdb9292b34715e5cc
SaiLavanyaP <peddinti98lavanya@gmail.com>
2021-11-19 00:04:07 -0600

    added

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }))
 
 const connectionString = 'mongodb+srv://DbLavanya:Lash2398@cluster0.gv4rx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose = require("mongoose");
@@ -71,13 +85,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", function () {
   console.log("Connection to DB succeeded");
 });
-
 app.use("/", indexRouter);
 app.use("/dog", dogRouter);
 app.use("/users", usersRouter);
@@ -85,6 +104,10 @@ app.use("/selector", selectorRouter);
 app.use("/addmods", addmodsRouter);
 //app.use("/costume", costumeRouter);
 app.use("/resource", resourceRouter);
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
