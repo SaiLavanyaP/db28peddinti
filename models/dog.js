fa9fba2eefd6699e68e479d1ece7a39a1e06fd84
SaiLavanyaP <peddinti98lavanya@gmail.com>
2021-12-01 20:45:16 -0600

    added

--- models/dog.js
+++ models/dog.js
@@ -1,8 +1,14 @@
-const mongoose = require("mongoose")
-const dogSchema = mongoose.Schema({
-Brand: String,
-price: Number,
-color: String
-})
-module.exports = mongoose.model("dog",
-dogSchema)
+const mongoose = require("mongoose") 
+const DogSchema = mongoose.Schema({ 
+ Brand: {
+    type: String,
+    minlength: 7
+},
+ price: Number, 
+ color: {
+    type: String,
+    minlength: 3
+}, 
+}) 
+ 
+module.exports = mongoose.model("Dog", DogSchema)
--- routes/Dog.js
+++ routes/Dog.js
@@ -1,22 +1,31 @@
 var express = require('express');
+const dog_controlers= require('../controllers/dog');
 var router = express.Router();
-var dog_controlers = require('../controllers/dog');
+
+// A little function to check if we have an authorized user and continue on
+//or redirect to login.
+const secured = (req, res, next) => {
+  if (req.user){
+      return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Dog', { title: 'Search results for Dog ' });
-});
+router.get('/', dog_controlers.dog_view_all_Page);
 
 /* GET detail dog page */
 router.get('/detail', dog_controlers.dog_view_one_Page);
 
 /* GET create costume page */ 
-router.get('/create', dog_controlers.dog_create_Page); 
+router.get('/create',secured, dog_controlers.dog_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', dog_controlers.dog_update_Page);
+router.get('/update',secured, dog_controlers.dog_update_Page);
 
-/* GET create costume page */ 
-router.get('/delete', dog_controlers.dog_delete_Page); 
+/* GET delete costume page */ 
+router.get('/delete',secured, dog_controlers.dog_delete_Page); 
+ 
 
 module.exports = router;
